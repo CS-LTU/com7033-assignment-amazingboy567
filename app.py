d8226bb6cbe660a66482e0a9cc57eba9c8b5cd63
amazingboy567 <2411591@leedstrinity.ac.uk>
2025-08-01 01:04:33 +0100

    First Commit, kind of nervous :)

---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo import MongoClient
+
+#intialise flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+#MongoDB Set Up
+mongo_client = MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["patient_db"]
+patient_collection = mongo_db["records"]
+
+#The model used for the user
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
